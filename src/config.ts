94d0f325603b0c0d5d3fa7521dcabb22190b8d4f
Yasushi Sakai <yasushi.accounts@fastmail.com>
2022-06-25 02:02:52 -0400

    changed config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 
 const config = {
-    endpoint: "http://127.0.0.1:8899",
+    // endpoint: "http://127.0.0.1:8899",
+    endpoint: "https://api.devnet.solana.com",
     programId: "HtZAJbfvjxWGgMSQh9Aht28sPNX1EC5jsGoeoq9DDqkA",
     groupName: "csdemo",
     admin: "DrPpPs73Bt8sh6Fbfb7v5hJUdJqER25AU2qjtgdqvwVk"
